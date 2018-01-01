a89e7bf921a3a917cffaac340f768c5c4f104520
Areschen <areschentw@outlook.com>
2018-01-01 22:21:53 +0800

    2017/12/31的練習檔

---
+++ TensorFlow/Tensorflow_TaskImage.py
@@ -0,0 +1,12 @@
+import matplotlib.image as mp_image
+import matplotlib.pyplot as plt
+filename = "packt.png"
+input_image = mp_image.imread(filename)
+
+print('input dim = {}'.format(input_image.ndim))
+print('input shape = {}'.format(input_image.shape))
+
+plt.imshow(input_image)
+plt.show()
+
+
